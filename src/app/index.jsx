3889181022d3b4a6fdf9e42858ac9f128a370cb5
jwilliams140 <jwilliams140@csub.edu>
2026-04-06 16:26:53 -0700

    Main screen updated to be a profile creation screen

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 
-const index = () => {
+export default function Index() {
+  // Holds onto the user name
+  const [name, setName] = useState('');
+
+  /*
+    This function logs the name only but doesn't save profiles just yet.
+  */
+  const handleCreateProfile = () => {
+    console.log('Profile name entered:', name);
+  };
+
   return (
-    <View>
-      <Text>index page!</Text>
+    <View style={{ padding: 100 }}>
+      {/* Game Title */}
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>
+        Untitled Story Driven Game
+      </Text>
+
+      {/* Section title */}
+      <Text>Create Your Profile</Text>
+
+      {/* Player Name Input */}
+      <TextInput
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+        style={{
+          borderWidth: 1,
+          padding: 8,
+          marginVertical: 10,
+        }}
+      />
+
+      {/* Create profile button that doesn't press for now */}
+      <Button title="Create Profile" onPress={handleCreateProfile} />
     </View>
-  )
+  );
 }
-
-export default index
-
-const styles = StyleSheet.create({})
