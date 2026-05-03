56697307a9fc05f4f988600c707300da95cda0b1
Nebo Paul <nebopaul@Mac.lan>
2026-05-03 13:51:20 -0700

    third charater can be interacted with, fixed broken backgrounds

--- src/constants/backgroundConfigs.js
+++ src/constants/backgroundConfigs.js
@@ -1,4 +1,5 @@
 export const SCENE_BACKGROUND_MAP = {
   scene1: require('../assets/backgrounds/Background_0.png'),
   scene4: require('../assets/backgrounds/Battleground1.png'),
+  scene2: require('../assets/backgrounds/scene2-bg.png'),
 };
